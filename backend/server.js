fc010dfc1c55f9eb910152bb7ea6a73b357432b1
monkinng9 <t_chayut@kkumail.com>
2022-06-18 14:43:38 +0700

    REST api and route

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config;
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// app.get('/api/goals', (req, res) => {
+//     res.status(200).json({ message: 'Get Goals'})
+// })
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
