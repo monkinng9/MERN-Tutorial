7252ef2279cd7cef439316b0116f815b61c712c1
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-06-20 23:56:18 +0700

    Create Goal Extra Reducer

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,29 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getGoals, reset } from '../features/goals/goalSlice'
+import GoalForm from '../components/GoalForm';
+
 function Dashboard() {
+  const navigate = useNavigate()
+  
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+
   return (
-    <div>Dashboard</div>
+    <>
+      <section className="heading">
+        <h1>Welcom {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+        <GoalForm/>
+      </section>
+    </>
   )
 }
 
